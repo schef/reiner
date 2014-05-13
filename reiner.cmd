542589b1a65a8dcca036ac20d707365df84bd00d
Stjepan Horvat <zvanstefan@gmail.com>
2014-05-13 09:45:56 +0200

    sve kaj sam imal od reinera

---
+++ reiner.cmd
@@ -0,0 +1,158 @@
+# Pin name action command file
+
+# Start of element Q1
+ChangePinName(Q1, 2, 2)
+ChangePinName(Q1, 1, 1)
+ChangePinName(Q1, 3, 3)
+
+# Start of element CONN3
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+
+# Start of element S4
+ChangePinName(S4, 1, 1)
+ChangePinName(S4, 2, 2)
+
+# Start of element S3
+ChangePinName(S3, 1, 1)
+ChangePinName(S3, 2, 2)
+
+# Start of element S2
+ChangePinName(S2, 1, 1)
+ChangePinName(S2, 2, 2)
+
+# Start of element S1
+ChangePinName(S1, 1, 1)
+ChangePinName(S1, 2, 2)
+
+# Start of element LED3
+ChangePinName(LED3, 2, K)
+ChangePinName(LED3, 1, A)
+
+# Start of element LED2
+ChangePinName(LED2, 2, K)
+ChangePinName(LED2, 1, A)
+
+# Start of element LED1
+ChangePinName(LED1, 2, K)
+ChangePinName(LED1, 1, A)
+
+# Start of element CONN1
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element U3
+ChangePinName(U3, 10, 10)
+ChangePinName(U3, 9, 9)
+ChangePinName(U3, 8, 8)
+ChangePinName(U3, 7, 7)
+ChangePinName(U3, 4, 4)
+ChangePinName(U3, 3, 3)
+ChangePinName(U3, 2, 2)
+ChangePinName(U3, 1, 1)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element D5
+ChangePinName(D5, 2, 2)
+ChangePinName(D5, 1, 1)
+
+# Start of element D4
+ChangePinName(D4, 2, 2)
+ChangePinName(D4, 1, 1)
+
+# Start of element D3
+ChangePinName(D3, 2, 2)
+ChangePinName(D3, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, 1, A)
+ChangePinName(U2, 2, B)
+ChangePinName(U2, 3, Y)
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, 4, A)
+ChangePinName(U2, 5, B)
+ChangePinName(U2, 6, Y)
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, 9, A)
+ChangePinName(U2, 10, B)
+ChangePinName(U2, 8, Y)
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, unknown, unknown)
+ChangePinName(U2, 12, A)
+ChangePinName(U2, 13, B)
+ChangePinName(U2, 11, Y)
+
+# Start of element U1
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, 1, A)
+ChangePinName(U1, 2, B)
+ChangePinName(U1, 3, Y)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, 12, A)
+ChangePinName(U1, 13, B)
+ChangePinName(U1, 11, Y)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, 9, A)
+ChangePinName(U1, 10, B)
+ChangePinName(U1, 8, Y)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, 4, A)
+ChangePinName(U1, 5, B)
+ChangePinName(U1, 6, Y)
